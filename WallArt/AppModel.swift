5b69e077dc79d918ab4e57af154f0b714393eeed
Christopher J Roura <cjroura@gmail.com>
2024-06-13 16:33:09 -0400

    Initial Commit

---
+++ WallArt/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  WallArt
+//
+//  Created by Christopher J. Roura on 6/13/24.
+//
+
+import SwiftUI
+
+/// Maintains app-wide state
+@MainActor
+@Observable
+class AppModel {
+    let immersiveSpaceID = "ImmersiveSpace"
+    enum ImmersiveSpaceState {
+        case closed
+        case inTransition
+        case open
+    }
+    var immersiveSpaceState = ImmersiveSpaceState.closed
+}
---
+++ WallArt/WallArtApp.swift
@@ -0,0 +1,33 @@
+//
+//  WallArtApp.swift
+//  WallArt
+//
+//  Created by Christopher J. Roura on 6/13/24.
+//
+
+import SwiftUI
+
+@main
+struct WallArtApp: App {
+
+    @State private var appModel = AppModel()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(appModel)
+        }
+
+        ImmersiveSpace(id: appModel.immersiveSpaceID) {
+            ImmersiveView()
+                .environment(appModel)
+                .onAppear {
+                    appModel.immersiveSpaceState = .open
+                }
+                .onDisappear {
+                    appModel.immersiveSpaceState = .closed
+                }
+        }
+        .immersionStyle(selection: .constant(.mixed), in: .mixed)
+     }
+}
